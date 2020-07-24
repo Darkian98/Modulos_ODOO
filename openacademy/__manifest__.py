5e2c6dd310ad40ce8e2aaf696e535a0eaad8ad7c
Darkian98 <Darkian98@users.noreply.github.com>
2020-07-24 06:40:05 -0600

    [NEW] ALL-WORK

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "Open Academy",
 
-    'summary': """Manage trainings""",
+    'summary': """Odoo Module Deneme""",
 
     'description': """
         Open Academy module for managing trainings:
@@ -11,8 +11,8 @@
             - attendees registration
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "USTA",
+    'website': "http://www.ugurcanusta.me",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -21,12 +21,17 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'templates.xml',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        'views/session_board.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
